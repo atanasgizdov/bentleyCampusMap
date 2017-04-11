55e6b737892e87fddf5b5ddb5648023a87293da9
Atanas Gizdov <agizdov@passkey.com>
2017-04-11 14:41:02 -0400

    Added loading screen, map intent, custom theme and DB wireframe classes

--- app/src/main/java/com/example/agizdov/bentleycampusmap/IntroPage.java
+++ app/src/main/java/com/example/agizdov/bentleycampusmap/IntroPage.java
@@ -4,6 +4,9 @@ package com.example.agizdov.bentleycampusmap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 /**
  * Created by agizdov on 4/11/2017.
@@ -22,7 +25,17 @@ public class IntroPage extends AppCompatActivity {
             public void run() {
                 try {
                     super.run();
+
+                    ImageView favicon = (ImageView) findViewById(R.id.intro_pic);
+                    RotateAnimation r = new RotateAnimation(0,360,
+                            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                    r.setDuration((long) 2*1500);
+                    r.setRepeatCount(5);
+                    favicon.startAnimation(r);
+
                     sleep(10000);  //Delay of 10 seconds
+
+
                 } catch (Exception e) {
 
                 } finally {
